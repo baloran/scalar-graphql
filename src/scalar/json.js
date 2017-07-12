4bc5e9d7998d8ffa28fedf265527a8042025536b
baloran <contact@baloran.fr>
2017-07-12 17:31:49 +0200

    fix(json scalar)

--- src/scalar/json.js
+++ src/scalar/json.js
@@ -1,6 +1,29 @@
 import { GraphQLScalarType } from 'graphql'
 import { Kind } from 'graphql/language'
 
+function parseJSONLiteral(ast) {
+  switch (ast.kind) {
+    case Kind.STRING:
+    case Kind.BOOLEAN:
+      return ast.value
+    case Kind.INT:
+    case Kind.FLOAT:
+      return parseFloat(ast.value)
+    case Kind.OBJECT: {
+      const value = Object.create(null)
+      ast.fields.forEach(field => {
+        value[field.name.value] = parseJSONLiteral(field.value)
+      })
+
+      return value
+    }
+    case Kind.LIST:
+      return ast.values.map(parseJSONLiteral)
+    default:
+      return null
+  }
+}
+
 export default new GraphQLScalarType({
   name: 'JSON',
   description: 'JSON custom scalar',
@@ -10,26 +33,5 @@ export default new GraphQLScalarType({
   serialize(value) {
     return value
   },
-  parseLiteral(ast) {
-    switch (ast.kind) {
-      case Kind.STRING:
-      case Kind.BOOLEAN:
-        return ast.value
-      case Kind.INT:
-      case Kind.FLOAT:
-        return parseFloat(ast.value)
-      case Kind.OBJECT: {
-        const value = Object.create(null)
-        ast.fields.forEach(field => {
-          value[field.name.value] = parseLiteral(field.value)
-        })
-
-        return value
-      }
-      case Kind.LIST:
-        return ast.values.map(parseLiteral)
-      default:
-        return null
-    }
-  },
+  parseLiteral: parseJSONLiteral,
 })
