2f16a7c21772bef97dc1ebfe4e72ea4402f2822c
baloran <contact@baloran.fr>
2017-09-13 18:54:10 +0200

    feat(arrayOrString): scalar for string or array of string

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import DateScalar from './scalar/date'
 import JSONScalar from './scalar/json'
 import StatusScalar from './scalar/status'
+import ArrayOrStringScalar from './scalar/arrayOrString'
 
 const resolverMap = {
   Date: DateScalar,
   JSON: JSONScalar,
   Status: StatusScalar,
+  ArrayOrString: ArrayOrStringScalar,
 }
 
 export default resolverMap
---
+++ src/scalar/arrayOrString.js
@@ -0,0 +1,21 @@
+import { GraphQLScalarType } from 'graphql'
+import { Kind } from 'graphql/language'
+
+export default new GraphQLScalarType({
+  name: 'ArrayOrString',
+  description: 'ArrayOrString.. explicit no ? :3',
+  parseValue(value) {
+    return value
+  },
+  serialize(value) {
+    return value
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.STRING) {
+      return ast.value
+    } else if (ast.kind === Kind.ARRAY) {
+      return ast.value
+    }
+    return null
+  },
+})
